5ccc262e42eb16d75049e5eb5f4d4491e16bc83d
DinhVietHoang07 <73330419+DinhVietHoang07@users.noreply.github.com>
2020-10-23 14:41:25 +0700

    Add files via upload

---
+++ Bài 2 vd2.c
@@ -0,0 +1,9 @@
+main ()
+{
+	char abc; /*abc of type character */
+	int xyz; /*xyz of typr integer */
+	float length; /*length of type float */
+	double are; /* are of type double */
+	long liteyrs; /*liteyrs of type long int */
+	short arm; /*arm of type short integer*/
+}	
