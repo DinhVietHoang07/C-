1fe99bd1d16c722c4757a8b5bfedd7784d0f5d3b
DinhVietHoang07 <73330419+DinhVietHoang07@users.noreply.github.com>
2020-11-05 15:22:01 +0700

    Add files via upload

---
+++ Bai 15-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+main()
+{
+	register int i;
+	int no, digit, sum;
+		printf("\nThe numbers whose sum of Cubes of Digits is Equal to th nymber itself are:\n\n");
+		
+		for(i=1;i<999;i++)
+		{
+			sum = 0;
+			no = i;
+			while(no)
+				{
+					digit = no%10;
+					no = no/10;
+					sum = sum + digit * digit * digit ;
+				}
+				if(sum == 1)
+				printf("t%d\n", i);
+		}
+}
---
+++ Bai 16-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+	int a, b, c, sum;
+	
+	printf("\nEnter any three number: ");
+	scanf("%d %d %d", &a, &b, &c);
+	
+	sum = calculatesum(a, b, c);
+	
+	printf("\nSum = %d", sum);
+}
+
+calcutesum(int x, int y, int z)
+{
+	int d;
+	
+	d = x + y + z;
+	
+}
